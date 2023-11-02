a21cc8d2f877a6a73e44dc7344ae90ea09606673
Marwin <m.pinedag2014@gmail.com>
2023-11-02 18:36:20 -0500

    🔧 [CHANGE] split the code in multiple bundles

---
+++ src/raspberry.js
@@ -0,0 +1,7 @@
+import Heading from "./components/heading/heading";
+import RaspberryImage from "./components/raspberry-image/raspberry-image";
+
+const heading = new Heading();
+heading.render();
+const raspberryImage = new RaspberryImage();
+raspberryImage.render();
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,9 +4,12 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    "hello-world": "./src/hello-world.js",
+    raspberry: "./src/raspberry.js",
+  },
   output: {
-    filename: "bundle.[contenthash].js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
     publicPath: "",
     clean: true,
@@ -61,7 +64,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "styles.[contenthash].css",
+      filename: "[name].[contenthash].css",
     }),
     new HtmlWebpackPlugin({
       title: "Hello Webpack",
