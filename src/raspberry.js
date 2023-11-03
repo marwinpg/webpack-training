1921f11498d5526a5e36ffb2738d5d4f897ad888
Marwin <m.pinedag2014@gmail.com>
2023-11-03 11:31:18 -0500

    ✨ [ADD] express with multiple page application

--- src/raspberry.js
+++ src/raspberry.js
@@ -1,6 +1,5 @@
 import Heading from "./components/heading/heading";
 import RaspberryImage from "./components/raspberry-image/raspberry-image";
-import React from "react";
 
 const heading = new Heading();
 heading.render("raspberry");
