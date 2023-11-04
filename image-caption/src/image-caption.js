6012f5b27a27e21e11d763975cc0b118c671fd56
Marwin <m.pinedag2014@gmail.com>
2023-11-03 19:42:11 -0500

    ✨ [ADD] image caption bundle

---
+++ image-caption/src/image-caption.js
@@ -0,0 +1,6 @@
+import ImageCaption from "./components/image-caption/image-caption.js";
+
+const imageCaption = new ImageCaption();
+imageCaption.render(
+  "The raspberry is the edible fruit of a multitude of plant species in the genus Rubus of the rose family"
+);
